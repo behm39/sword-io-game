ac72d7f1d066d89a4563db1cfa5a090810a43aff
michael <behm.39@osu.edu>
2020-04-20 22:47:25 -0400

    rendering simple players

--- public/game/sketch.js
+++ public/game/sketch.js
@@ -1,10 +1,25 @@
-var socket = io();
+var io = io();
+var stateHandler;
 
 function setup() {
+    stateHandler = new StateHandler();
     createCanvas(400, 400);
 }
 
 function draw() {
+    let state = stateHandler.getCurrentState();
+    
+
     background(201);
+    renderPlayer(state.me, true);
+    for (let i = 0; i < state.others.length; i++) {
+        renderPlayer(state.others[i]);
+    }
+
 }
 
+function renderPlayer(player, thisPlayer=false) {
+    stroke(0);
+    thisPlayer ? fill(255) : fill(0);
+    ellipse(player.x, player.y, 32);
+}
---
+++ public/game/state.js
@@ -0,0 +1,17 @@
+class StateHandler {
+    constructor() {
+
+    }
+    getCurrentState() {
+        // TODO: implement me
+        return {
+            t: 42,
+            me: { x: 200, y: 200, },
+            others: [
+                { x: 150, y: 100 },
+                { x: 300, y: 350 }
+            ]
+        };
+    }
+}
+
